60cd34e74525d49d4868373561cf9ce349d9732c
Kai Hong <ricedev10@gmail.com>
2025-10-31 13:57:10 -0400

    add buttons for calculator

---
+++ calculator-display.js
@@ -0,0 +1,17 @@
+const digitsMenu = document.querySelector(
+	".calculator-container .digits"
+);
+
+const DIGITS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// create the digits
+{
+	for (digit of DIGITS) {
+		// create the button for the digit and add to display
+		const button = document.createElement("button");
+		button.textContent = digit.toString();
+		button.setAttribute("digit", digit);
+
+		digitsMenu.appendChild(button);
+	}
+}
--- calculator-script.js
+++ calculator-script.js
@@ -1,15 +1,51 @@
-console.log("hello world!");
+"use strict";
+
+// keep track of digits
+let digitA = "0";
+let digitB = null;
+let operation = null;
+
+const digitButtons = document.querySelectorAll(
+	".calculator-container .digits > *"
+);
+for (let button of digitButtons) {
+	let value = button.getAttribute("digit");
+	button.addEventListener("click", () => {
+		console.log(digitA);
+		if (!digitB) {
+			digitA = `${digitA == "0" ? "" : digitA}${value}`;
+		}
+	});
+}
+
+// update display
 
 // calculator functions
-function add(a, b) {
-	return a + b;
-}
-function subtract(a, b) {
-	return a - b;
-}
-function multiply(a, b) {
-	return a * b;
-}
-function divide(a, b) {
-	return a / b;
+const ORDER_OF_OPERATIONS = [
+	"multiplication",
+	"division",
+	"addition",
+	"subtraction",
+];
+
+let numbers = [];
+
+let operations = {
+	add: function (a, b) {
+		return a + b;
+	},
+	subtract: function (a, b) {
+		return a - b;
+	},
+	multiply: function (a, b) {
+		return a * b;
+	},
+	divide: function (a, b) {
+		return a / b;
+	},
+};
+
+//
+function calculate(operator, a, b) {
+	return operations[operations](a, b);
 }
